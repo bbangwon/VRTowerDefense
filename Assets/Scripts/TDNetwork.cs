6de35bb3754dc7cdff89984a82f4afc98badb8f9
bbangwon <bbangwon.chung@gmail.com>
2019-11-18 02:12:16 +0900

    Step_7 PUN2 Asset 추가 AppID 094f0356-98a4-4976-b507-4fcd4732b409 설정

---
+++ Assets/Scripts/TDNetwork.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TDNetwork : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {       
+        if (PhotonNetwork.IsConnected)
+        {
+            //이미 연결되어 있을경우 룸에 접속 시도
+            if (PhotonNetwork.InRoom)
+                GameStart();
+            else
+                PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            //포톤 클라우드에 접속
+            PhotonNetwork.ConnectUsingSettings();
+        }                
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("새로 접속하여 룸 입장을 시도합니다.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room이 없어 새로 생성합니다.");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("룸에 입장했습니다.");
+        GameStart();
+    }
+
+    public void GameStart()
+    {
+        Debug.Log("게임을 시작합니다.");
+    }
+
+
+
+
+
+}

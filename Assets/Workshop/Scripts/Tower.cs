16e3d04583337cabddd626db0712c0498e3eb384
bbangwon <bbangwon.chung@gmail.com>
2019-11-18 00:21:00 +0900

    Step_1 프로젝트 생성, 리소스 임포트

---
+++ Assets/Workshop/Scripts/Tower.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Tower : MonoBehaviour {
+
+	public static Tower Instance;
+
+	public int MAX_HP = 10;
+	int hp = 0;
+
+	public GameObject die;
+
+	void Awake()
+	{
+        
+		if(Instance == null)
+			Instance = this;
+	}
+
+	void Start()
+	{
+        
+		hp = MAX_HP;
+	}
+
+	public void Damage()
+	{
+		hp--;
+
+		if(hp <= 0)
+		{
+			if(die)
+			{
+				die.SetActive(true);
+			}
+		}
+	}
+}
